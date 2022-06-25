0bc8fa7d8feb7e17871c0e08576e3bbca14d13f4
Lciarallo <85714365+Lciarallo@users.noreply.github.com>
2022-06-25 17:48:59 -0300

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 
+void moreop();
+void extras();
+int operation;
+
 int main(){
-    int operation;
+
     double num1;
     double num2;
     double result;
@@ -28,8 +32,11 @@ int main(){
     printf("Insert value of Y: ");
         scanf("%lf", &num2);
     }
-    switch (operation)
-    {
+    switch (operation){
+    
+    case 0:
+        puts("Extra");
+        moreop();
     case 1:
         result = num1 + num2;
         printf("The result of the operation %.2f + %.2f = %.2f", num1, num2, result);
@@ -55,8 +62,12 @@ int main(){
         printf("%.2f to the power of %.2f = %.2f", num1, num2, result);
         break;
     }
-if (operation == 0)
-{
+
+    return 0;
+}
+void moreop(){ 
+
+    
     puts("1 - Area");
     puts("2 - Circunference");
     puts("3 - Hypotenuse");
@@ -65,15 +76,53 @@ if (operation == 0)
     puts("6 - Cos");
     puts("7 - Tan");
     puts("8 - Logaritm");
+    puts("9 - return");
     puts("0 - Extras (Beta)");
     printf("\nWhat type of operation would you like to do? ");
     scanf("%1d", &operation);
+
+    switch{
+        case 0:{
+            extras();
+            break;
+        }
+        case 1:{
+
+        }
+        case 2:{
+
+        }
+        case 3:{
+
+        }
+        case 4:{
+
+        }
+        case 5:{
+
+        }
+        case 6:{
+
+        }
+        case 7:{
+
+        }
+        case 8:{
+
+        }
+        case 9:{
+            main();
+            break;
+        }
+    }
+
+    
 }
-if (operation == 0)
-{
-    puts("1 - Temperature Conversor");
-    puts("2 - Measurement Conversor");
-    puts("3 - Currency Conversor");
-}
-    return 0;
+
+void extras(){
+        puts("1 - Temperature Conversor");
+        puts("2 - Measurement Conversor");
+        puts("3 - Currency Conversor");
+        printf("\nWhat type of operation would you like to do? ");
+        scanf("%1d", &operation);
 }
